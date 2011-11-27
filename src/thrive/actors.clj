66d506b0fbae2b9e3861657c0cab97519a80dc5e
Maarten Hus <MaartenHus@gmail.com>
2011-11-27 21:03:28 +0100

    Put the actor code in seperate namespace.

---
+++ src/thrive/actors.clj
@@ -0,0 +1,34 @@
+(ns thrive.actors
+  (:gen-class))
+
+(defprotocol Actor
+"Defines what an Actor should respond to."
+  (live     [this] "The live method, IE what the actor does")
+  (interval [this] "The interval in which the actor performs live"))
+
+(defn loop-actor
+  "This starts the actors loop. It performs the actors 'live' function each 'interval'"
+  [actor]
+  (do
+    (. Thread sleep (interval actor))
+    (send-off *agent* loop-actor))
+  (live actor))
+
+(defrecord Person
+[
+  x 	  ;; The x position on the world 
+  y 	  ;; The y position on the world
+  z	    ;; The z position on the world, is equal to height of the tile.
+  food  ;; The current food this person is carrying.
+  w     ;; The current world as this person sees it.
+]
+  Actor
+  (live 
+    [this]
+    (do
+      (println "living the good life")
+      this))
+
+  (interval 
+    [this] 1000))
+ 
--- src/thrive/core.clj
+++ src/thrive/core.clj
@@ -1,4 +1,7 @@
-(ns thrive.core)
+(ns thrive.core
+  (:use thrive.actors)
+  (:import (thrive.actors Person))
+  (:gen-class))
 
 ;;;;;;; Records ;;;;;;;
 
@@ -10,31 +13,6 @@
  	tile 	;; The type of tile, in symbol form. IE :grass, :sea, :mountain
 	food  ;; The current food value of this cell
 ])
-
-;;;;;;; Protocols ;;;;;;;
-
-(defprotocol Actor
-"Defines what an Actor should respond to."
-  (live     [this] "The live method, IE what the actor does")
-  (interval [this] "The interval in which the actor performs live"))
-  
-(defrecord Person
-[
-  x 	  ;; The x position on the world 
-  y 	  ;; The y position on the world
-  z	    ;; The z position on the world, is equal to height of the tile.
-  food  ;; The current food this person is carrying.
-  w     ;; The current world as this person sees it.
-]
-  Actor
-  (live 
-    [this]
-    (do
-      (println "living the good life")
-      this))
-  
-  (interval 
-    [this] 1000))  
 
 ;;;;;;; References ;;;;;;;
 
@@ -54,14 +32,6 @@
    :actors
     [(agent (Person. 0 1 0 5 unkown-world))]
   }))
-
-(defn loop-actor
-  "This starts the actors loop. It performs the actors 'live' function each 'interval'"
-  [actor]
-  (do
-    (. Thread sleep (interval actor))
-    (send-off *agent* loop-actor))
-  (live actor))
 
 (defn live-world
   [w]  
