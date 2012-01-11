92b56b7421a80117c4fe32f23a4fc0a5b8f8127d
Maarten Hus <MaartenHus@gmail.com>
2012-01-11 12:17:45 +0100

    Broke up the human and the drawer. Makes things a little easier to manage.

--- src/thrive/core.clj
+++ src/thrive/core.clj
@@ -28,9 +28,9 @@
 
 (def world (ref 
   {:cells 
-    [(Cell. 0 0 0 :grass 0), (Cell. 1 0 0 :grass 15),    (Cell. 2 0 0 :grass 15), 
-     (Cell. 0 1 0 :sea 0), (Cell. 1 1 0 :sea 0),      (Cell. 2 1 0 :mountain 0),
-     (Cell. 0 2 0 :sea 0), (Cell. 1 2 0 :mountain 0), (Cell. 2 2 0 :lava 0)]
+    [(Cell. 0 0 0 :grass 0),  (Cell. 1 0 0 :grass 15),    (Cell. 2 0 0 :grass 15), 
+     (Cell. 0 1 0 :sea 0),    (Cell. 1 1 0 :sea 0),       (Cell. 2 1 0 :mountain 0),
+     (Cell. 0 2 0 :sea 0),    (Cell. 1 2 0 :mountain 0),  (Cell. 2 2 0 :lava 0)]
    
    :actors
     [(agent (City. 0 0 0 50 unknown-world)) (agent (Human. 0 1 0 5 unknown-world))]
