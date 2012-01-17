8f37826e967706da79505a986aab623cfc5a319d
Maarten Hus <MaartenHus@gmail.com>
2012-01-17 19:13:03 +0100

    Improved cost-path. Added value for method in test.

--- src/thrive/astar/astar.clj
+++ src/thrive/astar/astar.clj
@@ -2,8 +2,7 @@
   (:use [thrive.human])
   (:use [thrive.cell])
   (:import (thrive.cell Cell)))
-
-
+  
 (defn manhattan-distance 
   "http://en.wikipedia.org/wiki/Taxicab_geometry"
   [[x1 y1] [x2 y2]]
@@ -11,16 +10,14 @@
 
 (defn cost-path
   "Calculate the cost of the path as the name suggets"
-  [seq traversable]
-  (if (empty? seq)
-    0
-    (+ ((:tile (first seq)) traversable) (cost-path (rest seq) traversable))))
-
+  [coll traversable]
+  (reduce #(+ %1 ((:tile %2) traversable)) 0 coll))
+  
 ;(defn euclidian-distance [a b] ; multidimensional
 ;  (Math/sqrt (reduce + (map #(let [c (- %1 %2)] (* c c)) a b))))
 
 (defn cost 
-  "g(x) - cost of getting to that node from starting node. 
+  " g(x) - cost of getting to that node from starting node. 
     h(x) - cost of getting to the goal node from current node.
     f(x) - g(x)+h(x) "
   [seq end traversable]
@@ -30,7 +27,7 @@
     [f g h]))
 
 (defn get-path-a*
-  "Calcuate a path from the current point to the finish point on the given world"
+  "Calculate a path from the current point to the finish point on the given world"
   [[x1 y1] [x2 y2] movement traversable world world-size]
   (map #() ))
 
--- test/thrive/test/astar.clj
+++ test/thrive/test/astar.clj
@@ -18,7 +18,6 @@
    (Cell. 0 1 0 :lava 0),  (Cell. 1 1 0 :grass 0), (Cell. 2 1 0 :lava 0),
    (Cell. 0 2 0 :lava 0),  (Cell. 1 2 0 :lava 0),  (Cell. 2 2 0 :lava 0)])
 
-  
 (deftest test-manhattan-distance
   (is (= (manhattan-distance [0, 0] [0, 5]) 5 ))
   (is (= (manhattan-distance [0, 0] [5, 1]) 6 ))
@@ -37,23 +36,22 @@
   )
 
 (deftest test-cost-path
-  (is (= (cost-path [(Cell. 0 0 0 :grass 0) (Cell. 0 0 0 :grass 0) (Cell. 0 0 0 :grass 0)] traversable) 3))
-  (is (= (cost-path [(Cell. 0 0 0 :grass 0) (Cell. 0 0 0 :sand 0)] traversable) 3))
-  )
+  (is (= 3 (cost-path [(Cell. 0 0 0 :grass 0) (Cell. 0 0 0 :grass 0) (Cell. 0 0 0 :grass 0)] traversable)))
+  (is (= 3 (cost-path [(Cell. 0 0 0 :grass 0) (Cell. 0 0 0 :sand 0)] traversable))))
 
 (deftest test-cost
-  (is (= (cost [(Cell. 0 0 0 :grass 0)] [1 0] traversable ) [2 1 1]))
-  (is (= (cost [(Cell. 0 0 0 :grass 0) (Cell. 1 0 0 :grass 0)] [9 0] traversable ) [10 2 8]))
-  (is (= (cost [(Cell. 0 0 0 :grass 0) (Cell. 1 0 0 :sand 0)] [9 0] traversable ) [11 3 8]))
-  )
+  (is (= [2 1 1]  (cost [(Cell. 0 0 0 :grass 0)] [1 0] traversable)))
+  (is (= [10 2 8] (cost [(Cell. 0 0 0 :grass 0) (Cell. 1 0 0 :grass 0)] [9 0] traversable)))
+  (is (= [11 3 8] (cost [(Cell. 0 0 0 :grass 0) (Cell. 1 0 0 :sand 0)] [9 0] traversable))))
 
+(comment
 (deftest test-a*
-  ;(is (= (get-path-a* [1 6] [1 1] traversable movement simple-world simple-world-size) [[1 5], [1, 4], [1, 3], [1,2] [1, 1]]))
-  ;(is (= (get-path-a* [1 1] [1 6] traversable simple-world) [[1 2], [1, 2], [1, 3], [1,4] [1, 6]]))
+  (is (= [[1 5], [1 4], [1 3], [1 2] [1 1]] (get-path-a* [1 6] [1 1] traversable movement simple-world simple-world-size)))
+  (is (= [[1 2], [1 2], [1 3], [1 4] [1 6]] (get-path-a* [1 1] [1 6] traversable simple-world)))
 
-  ;(is (= (get-path-a* [2 1] [2 6] traversable round-world) [[1, 1] [1, 2] [1, 3] [1, 4] [1, 5] [1, 6] [2, 6]]))
-  ;(is (= (get-path-a* [2 6] [2 1] traversable round-world) [[1, 6] [1, 5] [1, 4] [1, 3] [1, 2] [1, 1] [2, 1]]))
+  (is (= [[1, 1] [1, 2] [1, 3] [1, 4] [1, 5] [1, 6] [2, 6]] (get-path-a* [2 1] [2 6] traversable round-world)))
+  (is (= [[1, 6] [1, 5] [1, 4] [1, 3] [1, 2] [1, 1] [2, 1]] (get-path-a* [2 6] [2 1] traversable round-world)))
   
   ;(is (= (get-path-a* [1 1] [1 2] traversable puzzle-world) []))
   ;(is (= (get-path-a* [1 1] [1 2] traversable puzzle-world) []))
-  ) 
+  )) 
