38e2e92473f8d5f76b1a77ee8a5785b4d7e91bbe
Danny <DeDanny@gmail.com>
2012-01-17 11:45:54 +0100

    helper function for a* and tests

--- src/thrive/astar/astar.clj
+++ src/thrive/astar/astar.clj
@@ -1,4 +1,7 @@
-(ns thrive.astar.astar)
+(ns thrive.astar.astar
+  (:use [thrive.human])
+  (:use [thrive.cell])
+  (:import (thrive.cell Cell)))
 
 
 (defn manhattan-distance 
@@ -6,18 +9,27 @@
   [[x1 y1] [x2 y2]]
   (+ (Math/abs ^Integer (- x2 x1)) (Math/abs ^Integer (- y2 y1))))
 
+(defn cost-path
+  "Calculate the cost of the path as the name suggets"
+  [seq traversable]
+  (if (= (count seq) 0)
+    0
+    (+ ((:tile (first seq)) traversable) (cost-path (rest seq) traversable))))
+
+;(defn euclidian-distance [a b] ; multidimensional
+;  (Math/sqrt (reduce + (map #(let [c (- %1 %2)] (* c c)) a b))))
+
 (defn cost 
   "g(x) - cost of getting to that node from starting node. 
     h(x) - cost of getting to the goal node from current node.
     f(x) - g(x)+h(x) "
-  [curr start end world]
-  (let [g (manhattan-distance start curr) 
-        h (manhattan-distance curr end)   
+  [seq end traversable]
+  (let [g (cost-path seq traversable) 
+        h (manhattan-distance (let [curr (last seq)] [(:x curr) (:y curr)]) end)   
         f (+ g h)]
     [f g h]))
 
 (defn get-path-a*
   "Calcuate a path from the current point to the finish point on the given world"
-  [[x1 y1] [x2 y2] traversable simple-world]
-  
-  )
+  [[x1 y1] [x2 y2] traversable world]
+  ())
--- test/thrive/test/astar.clj
+++ test/thrive/test/astar.clj
@@ -22,7 +22,20 @@
   (is (= (manhattan-distance [0, 0] [3, 3]) 6 ))
   (is (= (manhattan-distance [3, 3] [0, 0]) 6 ))
   (is (= (manhattan-distance [2, 2] [4, 4]) 4 ))
-  (is (= (manhattan-distance [4, 4] [2, 2]) 4 )))
+  (is (= (manhattan-distance [4, 4] [2, 2]) 4 ))
+  )
+
+(deftest test-cost-path
+  (is (= (cost-path [(Cell. 0 0 0 :grass 0) (Cell. 0 0 0 :grass 0) (Cell. 0 0 0 :grass 0)] traversable) 3))
+  (is (= (cost-path [(Cell. 0 0 0 :grass 0) (Cell. 0 0 0 :sand 0)] traversable) 3))
+  )
+
+(deftest test-cost
+  (is (= (cost [(Cell. 0 0 0 :grass 0)] [1 0] traversable ) [2 1 1]))
+  (is (= (cost [(Cell. 0 0 0 :grass 0)] [8 0] traversable ) [9 1 8]))
+  (is (= (cost [(Cell. 0 0 0 :grass 0)] [9 0] traversable ) [10 1 9]))
+  (is (= (cost [(Cell. 0 0 0 :grass 0) (Cell. 1 0 0 :grass 0)] [9 0] traversable ) [10 2 8]))
+  )
 
 (deftest test-a*
   ;(is (= (get-path-a* [1 6] [1 1] traversable simple-world) [[1 5], [1, 4], [1, 3], [1,2] [1, 1]]))
