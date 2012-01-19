dce58e232a01b5e4c730b4974bdf2d2fedd67076
Cornel Alders <cornel.alders@gmail.com>
2012-01-19 03:23:03 +0100

    Added planner which obtains plans for traversing the world from various pathfinding algorithms. Added test cases for MDP.

---
+++ test/thrive/test/planner.clj
@@ -0,0 +1,24 @@
+(ns thrive.test.planner
+  (:use [thrive.planner])
+  (:use [thrive.cell])
+  (:import (thrive.cell Cell))
+  (:use [clojure.test]))
+
+(def world-size 3)
+
+(def world
+  [(Cell. 0 0 0 :grass 0),  (Cell. 1 0 0 :grass 0),  (Cell. 2 0 0 :grass 0), 
+   (Cell. 0 1 0 :grass 0),  (Cell. 1 1 0 :lava 0),   (Cell. 2 1 0 :sea 0),
+   (Cell. 0 2 0 :city 0),   (Cell. 1 2 0 :sea 0),    (Cell. 2 2 0 :sea 0)])
+
+(def movement {:stay [0 0], :left [0 -1], :right [0 1], :up [-1 0], :down [1 0]})
+(def traversable {:city 1, :grass 1, :mountain 3, :sea 15, :desert 2, :lava false})
+
+(deftest get-plan-test
+  (is (=  [] (get-plan :mdp 0 0 0 0 movement traversable world world-size)))
+  (is (=  [[0 1] [0 0] [1 0] [2 0] [2 1]] (get-plan :mdp 0 2 2 1 movement traversable world world-size)))
+  (is (=  [[1 0] [2 0] [2 1] [2 2]] (get-plan :mdp 0 0 2 2 movement traversable world world-size)))
+  (is (= [] (get-plan :astar 0 0 2 2 movement traversable world world-size)))
+  ;(is (= [[1 0] [2 0] [2 1] [2 2]] (get-plan :astar 0 0 2 2 movement
+  ;traversable world world-size)))
+  )
