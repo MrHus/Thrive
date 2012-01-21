8c48c08c5783ee05de6da5be138c676add6d74b6
Maarten Hus <MaartenHus@gmail.com>
2012-01-21 14:01:00 +0100

    MDP now returns an empty vector on failure. MDP now runs value iteration a max number of times prevents infinite loops.

--- test/thrive/test/mdp.clj
+++ test/thrive/test/mdp.clj
@@ -54,14 +54,28 @@
   (assoc (Cell. 0 1 0 :unknown 0) :value 25),  (assoc (Cell. 1 1 0 :unknown 0) :value 50), (assoc (Cell. 2 1 0 :unknown 0), :value 75)
   (assoc (Cell. 0 2 0 :unknown 0) :value 0),  (assoc (Cell. 1 2 0 :unknown 0) :value 25), (assoc (Cell. 2 2 0 :unknown 0)  :value 50)])
 
+;; World surrounded by false twice removed
+(def test-world-5
+ [(Cell. 0 0 0 :lava 0),   (Cell. 1 0 0 :lava 0),  (Cell. 2 0 0 :grass 0), 
+  (Cell. 0 1 0 :grass 0),  (Cell. 1 1 0 :lava 0),  (Cell. 2 1 0 :lava 0),
+  (Cell. 0 2 0 :grass 0),  (Cell. 1 2 0 :grass 0), (Cell. 2 2 0 :grass 0)])
+
+;; World surrounded by false twice removed
+(def test-world-value-iterated-5
+ [(assoc (Cell. 0 0 0 :lava 0)  :value false),  (assoc (Cell. 1 0 0 :lava 0)  :value false), (assoc (Cell. 2 0 0 :grass 0), :value 100) 
+  (assoc (Cell. 0 1 0 :grass 0) :value false),  (assoc (Cell. 1 1 0 :lava 0)  :value false), (assoc (Cell. 2 1 0 :lava 0),  :value false)
+  (assoc (Cell. 0 2 0 :grass 0) :value false),  (assoc (Cell. 1 2 0 :grass 0) :value false), (assoc (Cell. 2 2 0 :grass 0)  :value false)])
+
 (deftest value-iteration-test
   (is (= test-world-value-iterated-1 (value-iteration 2 0 movement-mask traversable test-world-1 world-size)))
   (is (= test-world-value-iterated-2 (value-iteration 2 0 movement-mask traversable test-world-2 world-size)))
   (is (= test-world-value-iterated-3 (value-iteration 2 0 movement-mask traversable test-world-3 world-size)))
-  (is (= test-world-value-iterated-4 (value-iteration 2 0 movement-mask traversable test-world-4 world-size))))
+  (is (= test-world-value-iterated-4 (value-iteration 2 0 movement-mask traversable test-world-4 world-size)))
+  (is (= test-world-value-iterated-5 (value-iteration 2 0 movement-mask traversable test-world-5 world-size))))
   
 (deftest plan-test
-  (is (= '([0 1] [0 0] [1 0] [2 0]) (plan 0 2 2 0 movement traversable test-world-1 world-size)))
-  (is (= '([0 1] [0 0] [1 0] [2 0]) (plan 0 2 2 0 movement traversable test-world-2 world-size)))
-  (is (= '([0 1] [0 0] [1 0] [2 0]) (plan 0 2 2 0 movement traversable test-world-3 world-size)))
-  (is (= '([1 2] [2 2] [2 1] [2 0]) (plan 0 2 2 0 movement traversable test-world-4 world-size))))
+  (is (= [[0 1] [0 0] [1 0] [2 0]] (plan 0 2 2 0 movement traversable test-world-1 world-size)))
+  (is (= [[0 1] [0 0] [1 0] [2 0]] (plan 0 2 2 0 movement traversable test-world-2 world-size)))
+  (is (= [[0 1] [0 0] [1 0] [2 0]] (plan 0 2 2 0 movement traversable test-world-3 world-size)))
+  (is (= [[1 2] [2 2] [2 1] [2 0]] (plan 0 2 2 0 movement traversable test-world-4 world-size)))
+  (is (= []                        (plan 0 2 2 0 movement traversable test-world-5 world-size))))
